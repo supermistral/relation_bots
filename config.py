01686a5a55560b283db61f8c8ae6e68b104a6e1f
SuperMistral <mediamart37@gmail.com>
2020-10-17 00:26:04 +0300

    new

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Токен Телеграма
-tokenTelegram = ""
+tokenTelegram = "1345315357:AAH7eN2Rxyys6XZIjy7RsheivNUak4uxDsU"
 
 # Токен ВК (афиша)
-tokenVk = ""
+tokenVk = "3065c03cd80efe01b7caf2d5c650ccf219e73c8a54728cb9af85091c27fda9a2d8b599f2ea72ad35d475c"
 
 # База данных
 database = "BotDatabase.sqlite"
