174cc39a7ce23ac6de9c71014e3b27a487ecc78d
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-17 00:28:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Токен Телеграма
-tokenTelegram = "1345315357:AAH7eN2Rxyys6XZIjy7RsheivNUak4uxDsU"
+tokenTelegram = ""
 
 # Токен ВК (афиша)
-tokenVk = "3065c03cd80efe01b7caf2d5c650ccf219e73c8a54728cb9af85091c27fda9a2d8b599f2ea72ad35d475c"
+tokenVk = ""
 
 # База данных
 database = "BotDatabase.sqlite"
